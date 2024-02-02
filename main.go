d0c269973dbd10cfb0afcd981453f54eb9edcaf2
Evan <evanaltwarg2021@gmail.com>
2024-02-02 01:50:07 -0500

    complete Part 2: Drawing the Game Board

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
 const (
 	width              = 500
 	height             = 500
+	rows               = 10
+	columns            = 10
 	vertexShaderSource = `
     #version 430
     in vec3 vp;
@@ -30,11 +32,27 @@ const (
 	` + "\x00"
 )
 
+type cell struct {
+	drawable uint32
+
+	x int
+	y int
+}
+
 var (
 	triangle = []float32{
-		0, 0.5, 0, // top
-		-0.5, -0.5, 0, // left
-		0.5, -0.5, 0, // right
+		-0.5, 0.5, 0,
+		-0.5, -0.5, 0,
+		0.5, -0.5, 0,
+	}
+	square = []float32{
+		-0.5, 0.5, 0,
+		-0.5, -0.5, 0,
+		0.5, -0.5, 0,
+
+		-0.5, 0.5, 0,
+		0.5, 0.5, 0,
+		0.5, -0.5, 0,
 	}
 )
 
@@ -47,10 +65,10 @@ func main() {
 	defer glfw.Terminate()
 
 	program := initOpenGL()
-	vao := makeVao(triangle)
+	cells := makeCells()
 
 	for !window.ShouldClose() {
-		draw(vao, window, program)
+		draw(cells, window, program)
 	}
 }
 
@@ -97,12 +115,16 @@ func initOpenGL() uint32 {
 	return program
 }
 
-func draw(vao uint32, window *glfw.Window, program uint32) {
+func draw(cells [][]*cell, window *glfw.Window, program uint32) {
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 	gl.UseProgram(program)
 
-	gl.BindVertexArray(vao)
-	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle))/3)
+	for x := range cells {
+		for _, c := range cells[x] {
+			c.draw()
+		}
+	}
+
 	glfw.PollEvents()
 	window.SwapBuffers()
 }
@@ -145,4 +167,50 @@ func compileShader(source string, shaderType uint32) (uint32, error) {
 	}
 
 	return shader, nil
+}
+
+func makeCells() [][]*cell {
+	cells := make([][]*cell, rows, rows)
+	for x := 0; x < rows; x++ {
+		for y := 0; y < columns; y++ {
+			c := newCell(x, y)
+			cells[x] = append(cells[x], c)
+		}
+	}
+	return cells
+}
+
+func newCell(x, y int) *cell {
+	points := make([]float32, len(square), len(square))
+	copy(points, square)
+	for i := 0; i < len(points); i++ {
+		var pos float32
+		var size float32
+		switch i % 3 {
+		case 0:
+			size = 1.0 / float32(columns)
+			pos = float32(x) * size
+		case 1:
+			size = 1.0 / float32(rows)
+			pos = float32(y) * size
+		default:
+			continue
+		}
+		if points[i] < 0 {
+			points[i] = pos*2 - 1
+
+		} else {
+			points[i] = (pos+size)*2 - 1
+		}
+	}
+	return &cell{
+		drawable: makeVao(points),
+		x:        x,
+		y:        y,
+	}
+}
+
+func (c *cell) draw() {
+	gl.BindVertexArray(c.drawable)
+	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(square)/3))
 }
